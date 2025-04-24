2c246eadcfebccfdca577fe0e6390097636a5527
Sage Allen <gigyas@gmail.com>
2025-04-24 09:11:11 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿using System.Numerics;
+
+using Raylib_CSharp;
+using Raylib_CSharp.Colors;
+using Raylib_CSharp.Camera.Cam2D;
+using Raylib_CSharp.Collision;
+using Raylib_CSharp.Images;
+using Raylib_CSharp.Interact;
+using Raylib_CSharp.Textures;
+using Raylib_CSharp.Rendering;
+using Raylib_CSharp.Windowing;
+using Raylib_CSharp.Transformations;
+
+namespace HelloWorld;
+
+enum EntityType {
+    Player,
+    Asteroid,
+    SimpleBullet,
+}
+
+interface Entity {
+    void Update(GameState game, float dt, Vector2 ddP);
+}
+
+class SpatialEntity : Entity {
+    public EntityType type;
+    public Vector2 P, dP;
+    public float damping;
+    public float width, height;
+    public Vector2 forward;
+    public Texture2D tex;
+    public Rectangle srcRect;
+    public bool canCollide;
+    public float age, ttl;
+    public bool active;
+
+    public void Update(GameState game, float dt, Vector2 ddP){
+        age += dt;
+        if(ttl > 0 && age > ttl) {
+            active = false;
+            return;
+        }
+
+        float ddPLenth = RayMath.Vector2LengthSqr(ddP);
+        if(ddPLenth > 1.0f) {
+            ddPLenth *= (float)(1.0/Math.Sqrt(ddPLenth));
+        }
+
+        float entitySpeed = 2000.0f;
+        ddP = RayMath.Vector2Scale(ddP, entitySpeed);
+        ddP = RayMath.Vector2Add(ddP, RayMath.Vector2Scale(dP, damping));
+
+        Vector2 entityDelta = RayMath.Vector2Add(RayMath.Vector2Scale(ddP, 0.5f*dt*dt), RayMath.Vector2Scale(dP, dt));
+        dP = RayMath.Vector2Add(RayMath.Vector2Scale(ddP, dt), dP);
+
+        float tMin = 1.0f;
+        P = RayMath.Vector2Add(P, RayMath.Vector2Scale(entityDelta, tMin));
+
+        foreach(Entity target in game.entities) {
+            SpatialEntity? targetEnt = target as SpatialEntity;
+            if(targetEnt == null || !targetEnt.canCollide || this == targetEnt) {
+                continue;
+            }
+            if(ShapeHelper.CheckCollisionCircles(P, width/2, targetEnt.P, targetEnt.width/2)) {
+                float cDist = RayMath.Vector2Distance(P, targetEnt.P);
+                float overlap = 0.5f * (cDist -width/2 - targetEnt.width/2);
+                P.X -= overlap * (P.X - targetEnt.P.X)/cDist;
+                P.Y -= overlap * (P.Y - targetEnt.P.Y)/cDist;
+                targetEnt.P.X += overlap * (P.X - targetEnt.P.X)/cDist;
+                targetEnt.P.Y += overlap * (P.Y - targetEnt.P.Y)/cDist;
+            }
+        }
+    }
+}
+
+class GameState {
+    public List<Entity> entities;
+    public SpatialEntity playerEntity;
+
+    public GameState() {
+        Image ship = Image.Load("res/player_ship.png");
+        playerEntity = new SpatialEntity();
+        playerEntity.type = EntityType.Player;
+        playerEntity.tex = Texture2D.LoadFromImage(ship);
+        playerEntity.P = Vector2.Zero;
+        playerEntity.dP = Vector2.Zero;
+        playerEntity.damping = -2.0f;
+        playerEntity.canCollide = true;
+        playerEntity.forward = new Vector2(0,-1);
+        playerEntity.srcRect = new Rectangle(0,0, (float)ship.Width, (float)ship.Height);
+        playerEntity.width = (float)ship.Width;
+        playerEntity.height = (float)ship.Height;
+        playerEntity.age = 0;
+        playerEntity.ttl = -1;
+        playerEntity.active = true;
+        ship.Unload();
+
+        entities = new List<Entity>();
+        entities.Add(playerEntity);
+    }
+
+};
+
+class Game {
+    private const int SCREEN_WIDTH = 1280;
+    private const int SCREEN_HEIGTH = 720;
+
+    public static int Main() {
+        Window.Init(SCREEN_WIDTH, SCREEN_HEIGTH, "Hello World");
+        Time.SetTargetFPS(60);
+
+        GameState game = new GameState();
+        Vector2 halfScreen = new Vector2(SCREEN_WIDTH/2, SCREEN_HEIGTH/2);
+        Camera2D cam = new Camera2D(halfScreen, Vector2.Zero, 0, 1);
+        float rotSpeed = 9.0f;
+
+        while (!Window.ShouldClose()) {
+            float dt = Time.GetFrameTime();
+            float rotInput = 0.0f;
+
+            if(Input.IsKeyDown(KeyboardKey.D)) {
+                rotInput = rotSpeed * dt;
+            } else if(Input.IsKeyDown(KeyboardKey.A)) {
+                rotInput = -rotSpeed * dt;
+            }
+
+            // Update
+            foreach(Entity rawEnt in game.entities) {
+                SpatialEntity ent = (SpatialEntity)rawEnt;
+
+                switch(ent.type) {
+                    case EntityType.Player:
+                    ent.forward = RayMath.Vector2Rotate(ent.forward, rotInput);
+                        ent.Update(game, dt, Vector2.Zero);
+                        break;
+                }
+            }
+
+
+            Graphics.BeginDrawing();
+            Graphics.BeginMode2D(cam);
+            Graphics.ClearBackground(Color.Black);
+
+            foreach(Entity rawEnt in game.entities) {
+                SpatialEntity ent = (SpatialEntity)rawEnt;
+
+                switch(ent.type) {
+                    case EntityType.Player:
+                    float rotation = RayMath.Vector2Angle(ent.forward, new Vector2(0,-1));
+                    rotation *= -180/(float)Math.PI;
+                    float half = ent.srcRect.Height/2.0f;
+                    Graphics.DrawTexturePro(ent.tex, ent.srcRect, new Rectangle(ent.P.X, ent.P.Y, half*2, half*2), new Vector2(half, half), rotation, Color.White);
+                    break;
+                }
+            }
+
+
+
+            Graphics.EndMode2D();
+            Graphics.DrawFPS(10, 10);
+            Graphics.EndDrawing();
+        }
+
+        Window.Close();
+
+        return 0;
+    }
+}
